8ad9accb31d6fbf1ba4cb5decc34c28309c49202
Michal Franc <mfranc@email.cz>
2020-02-27 07:40:59 +0100

    [sxhkd] added shortcut for mute/unmute

---
+++ sxhkd/mute.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+status="$(amixer get Master | awk -F'[][]' '/%/{print $4;exit}')"
+
+if [ "$status" = "off" ]; then
+    amixer -q set Master unmute
+elif [ "$status" = "on" ]; then
+	amixer -q set Master mute
+fi
