5a63f7e92a8234afb5c1dcae8baaecd85025f372
m-fr <mfranc@email.cz>
2020-02-27 12:48:06 +0100

    [sxhkd] fixed name of audio mute key

--- sxhkd/mute.sh
+++ sxhkd/mute.sh
@@ -5,5 +5,5 @@ status="$(amixer get Master | awk -F'[][]' '/%/{print $4;exit}')"
 if [ "$status" = "off" ]; then
     amixer -q set Master unmute
 elif [ "$status" = "on" ]; then
-	amixer -q set Master mute
+    amixer -q set Master mute
 fi
