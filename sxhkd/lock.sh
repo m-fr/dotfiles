81eb2ef6771ad37cc3ce8ffdd2130ed80b038fab
m-fr <mfranc@email.cz>
2020-11-25 17:52:40 +0100

    improved lock screen

--- sxhkd/lock.sh
+++ sxhkd/lock.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
-i3lock -i ~/.wallpaper/lock.png -c 3d3d3d -f
+i3lock \
+    --nofork \
+    --show-failed-attempts \
+    --pass-media-keys \
+    --pass-volume-keys \
+    --image="/home/mfr/.wallpaper/lock.png" \
+    \
+    --color=3d3d3dff \
+    --verifcolor=00000000 \
+    --keyhlcolor=000000ff \
+    --ringcolor=3d3d3dff \
+    --ringvercolor=3d3d3dff \
+    --ringwrongcolor=e59d17ff \
+    --insidecolor=00000000 \
+    --insidevercolor=3d3d3d88 \
+    --insidewrongcolor=00000000 \
+    --timecolor=ffffffdd \
+    --datecolor=00000000 \
+    \
+    --force-clock \
+    --timestr="%I:%M" \
+    --timesize=125 \
+    --time-font="IBM Plex Sans Cond:style=Bold" \
+    --timepos="x+w*0,1:h*0,2" \
+    --time-align=1
