9b7e7e238812ade6d85334603f0facd3a48e9052
Michal Franc <mfranc@email.cz>
2023-09-04 08:44:41 +0200

    Updated application help and shortcuts

--- sxhkd/app_help.sh
+++ sxhkd/app_help.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-shortcuts=$HOME/.config/.shortcuts/
+shortcuts=$HOME/.shortcuts/
 app=`ls $shortcuts | rofi -dmenu -i -no-show-icons -width 1000`
 
 if [[ "$app" ]]; then
-    cat ~/.config/.shortcuts/$app | \
+    cat ~/.shortcuts/$app | \
     awk '/^[^#]/ && last {print $0,"\t",last} {last=""} /^#/{last=$0}' | \
     column -t -s $'\t' | \
     rofi -dmenu -i -no-show-icons -width 1000
