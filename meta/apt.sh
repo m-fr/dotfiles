3d3c999e713fdc37ccaef6ffcbd7cd12a54c75ac
Michal Franc <mfranc@email.cz>
2023-09-25 14:15:16 +0200

    Updated dependency install

--- meta/apt.sh
+++ meta/apt.sh
@@ -8,6 +8,9 @@ sudo apt-get update
 
 for item in $(cat $1)
 do
+    [[ -z "$item" ]] && continue
+    [[ "${item:0:1}" == "#" ]] && continue
+
     echo "Installing $item"
     sudo apt-get install -y $item || echo -e "\e[31m$item installation failed\e[0m"
 done
